d359f8818ae3c925e683a6fd00afeb04030d2f26
SgtPepper <augustlanguepin@gmail.com>
2025-06-18 20:07:19 +0700

    FIXED

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://cazucpeacpmqthpryoft.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhenVjcGVhY3BtcXRocHJ5b2Z0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyMjQyMzcsImV4cCI6MjA2NTgwMDIzN30.a4zDwsGweU0JtBda_pGjA6QbVFBdCFXaw7RkMoMD90o';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
